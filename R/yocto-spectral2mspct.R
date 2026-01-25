7338811e39382cbcdc00cb50842eeec5bb0cc662
Pedro Aphalo <pedro.aphalo@helsinki.fi>
2026-01-25 03:25:47 +0200

    Tweak for performance and docs

--- R/yocto-spectral2mspct.R
+++ R/yocto-spectral2mspct.R
@@ -3,15 +3,19 @@
 #' Convert a data frame with data imported with \code{read_yocto_spectral_csv()}
 #' into a colelction of \code{raw_spct} objects-
 #'
-#' @param x data.frame With only one of average, minimum or maximum sensor
-#'   counts.
+#' @param df data.frame With only one of average, minimum or maximum sensor
+#'   counts for the channels.
+#' @param channels character vector of length one or longer. In addition to a
+#'   vector of spectral sensor channel names as used in the AS7343
+#'   documentation, the nicknames \code{"all"}, \code{"wide"}, \code{"narrow"}
+#'   and \code{"xyz"} as vectors of length one are accepted.
 #'
 #' @export
 #'
-yocto_spectral2mspct <- function(x) {
+yocto_spectral2mspct <- function(df, channels = "all") {
 
-  if (is.null(channels)) {
-    channels <- "all"
+  if (is.null(channels) || channels == "all") {
+    channels <- names(AS7343_metadata())
   } else if (tolower(channels) == "xyz") {
     channels <- c("FZ", "FY", "FXL")
   } else if (channels == "wide") {
@@ -19,14 +23,28 @@ yocto_spectral2mspct <- function(x) {
   } else if (channels == "narrow") {
     channels <- paste("F", 1:8, sep = "")
   }
-  z <- tidyr::pivot_longer(x,
-                           cols = grep("time", colnames(x), invert = TRUE),
+
+  channels <- intersect(channels, names(AS7343_metadata()))
+  stopifnot("Invalid or missing 'channel' names!" = length(channels) >= 1L)
+
+  # sort channels by w.length before pivoting
+  channels <- channels[order(AS7343_metadata()[channels])]
+  # make sure colnames are bare channel names
+  colnames(df) <- gsub("\\.avg$", "", colnames(df))
+  # subset columns before pivoting
+  df <- df[ , c("time", channels)]
+  # convert to factor before pivoting
+  df[["time"]] <- factor(as.character(df[["time"]]))
+
+  message("Creating a 'source_mspct' containing ", length(levels(df[["time"]])),
+          " 'source_spct' objects.")
+
+  z <- tidyr::pivot_longer(df,
+                           cols = grep("time", colnames(df), invert = TRUE),
                            names_to = "channel",
                            values_to = "counts")
-  z[["channel"]] <- gsub("\\.avg$", "", z[["channel"]])
   z[["w.length"]] <- AS7343_metadata()[z[["channel"]]]
-  z <- z[order(z[["w.length"]])]
-  z[["time"]] <- factor(as.character(z[["time"]])) # better performance?
 
+  # this is currently rather slow!
   photobiology::subset2mspct(z, member.class = "raw_spct", idx.var = "time")
 }
--- data-raw/yocotopuce/plot-spct-large-data-pkg.R
+++ data-raw/yocotopuce/plot-spct-large-data-pkg.R
@@ -1,58 +1,57 @@
 library(ggspectra)
 library(dplyr)
 library(lubridate)
-library(AS7343)
+library(rYoctoPuceInOut)
 
 ## Data Lumitronix Q-36 LED array Auto-gain test
 # times are out of sync!!
 
-Q36_01.tb <-
+data1.df <-
   read_yocto_spectral_csv(file = "data-raw/yocotopuce/spectrl-01-2025-12-02.csv",
                           tz = "Europe/Helsinki")
 
-Q36_02.tb <-
+data2.df <-
   read_yocto_spectral_csv(file = "data-raw/yocotopuce/spectrl-02-2025-12-02.csv",
                           tz = "Europe/Helsinki")
 
-ggplot(Q36_01.tb, aes(time)) +
+ggplot(data1.df, aes(time)) +
   geom_point(aes(y = VIS.avg)) +
   geom_point(aes(y = VIS.avg), colour = "red") +
   geom_line(aes(y = VIS.avg)) +
   geom_line(aes(y = VIS.avg), colour = "red")
 
-ggplot(Q36_02.tb, aes(time)) +
+ggplot(data2.df, aes(time)) +
   geom_point(aes(y = VIS.avg)) +
   geom_point(aes(y = VIS.avg), colour = "red") +
   geom_line(aes(y = VIS.avg)) +
   geom_line(aes(y = VIS.avg), colour = "red")
 
-ggplot(Q36_01.tb, aes(time)) +
+ggplot(data1.df, aes(time)) +
   geom_point(aes(y = NIR.avg)) +
   geom_point(aes(y = NIR.avg), colour = "red") +
   geom_line(aes(y = NIR.avg)) +
   geom_line(aes(y = NIR.avg), colour = "red")
 
-ggplot(Q36_01.tb, aes(time)) +
+ggplot(data1.df, aes(time)) +
   geom_point(aes(y = F1.avg)) +
   geom_point(aes(y = F1.avg), colour = "red") +
   geom_line(aes(y = F1.avg)) +
   geom_line(aes(y = F1.avg), colour = "red")
 
-Q36.mspct <- yocto_spectral2mspct(Q36_01.tb, "narrow")
+data1_all.mspct <- yocto_spectral2mspct(data1.df)
 
-autoplot(Q36.mspct[[30]], geom = "col", range = c(350, 900))
-autoplot(Q36.mspct[[30]], geom = c("point", "line"), range = c(350, 900))
+autoplot(data1_all.mspct[[30]], geom = "col", range = c(350, 900))
+autoplot(data1_all.mspct[[30]], geom = c("point", "line"), range = c(350, 900))
 
-Q36.mspct <- yocto_spectral2mspct(Q36_01.tb, "wide")
+data1_narrow.mspct <- yocto_spectral2mspct(data1.df, "narrow")
 
-autoplot(Q36.mspct[[30]], geom = "col", range = c(250, 1000))
+autoplot(data1_narrow.mspct[[30]], geom = "col", range = c(350, 900))
+autoplot(data1_narrow.mspct[[30]], geom = c("point", "line"), range = c(350, 900))
 
-Q36.mspct <- yocto_spectral2mspct(Q36_01.tb, "xyz")
+data1_wide.mspct <- yocto_spectral2mspct(data1.df, "wide")
 
-autoplot(Q36.mspct[[30]], geom = "col", range = c(250, 1000))
+autoplot(data1_wide.mspct[[30]], geom = "col", range = c(250, 1000))
 
-Q36.mspct <- yocto_spectral2mspct(Q36_01.tb, "all")
+data1_xyz.mspct <- yocto_spectral2mspct(data1.df, "xyz")
 
-autoplot(Q36.mspct[[30]], geom = "col")
-autoplot(Q36.mspct[[30]], geom = c("point", "line"), range = c(350, 900))
-
+autoplot(data1_xyz.mspct[[30]], geom = "col", range = c(250, 1000))
